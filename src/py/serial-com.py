3056ed2d1d881f9d1a5a4d96cf02d012d44b5fcf
Ugo <ugo.briasco@sumup.com>
2019-10-23 00:08:16 +0200

    Serial comunication py-ino for motion

--- src/py/serial-com.py
+++ src/py/serial-com.py
@@ -1,12 +1,22 @@
 from time import sleep
-
 import serial
 
 ser = serial.Serial('/dev/tty.usbserial-1410',9600)
 
-while True:
+print "Hello"
 
+while 1:
     read_serial = ser.readline()
+    print "Reading:\n"
     print ser.readline() # Read the newest output from the Arduino
+    print "----"
     sleep(.1) # Delay for one tenth of a second
-    ser.write('1')
+    ##input = 'C10101'
+    ## ser.write(input.decode("hex"))
+    ###print "Sending:\n",input.decode("hex")
+
+    input ="1=10;2=99;"
+    ser.write(input)
+    print "Sending:\n",input
+
+    print "----"

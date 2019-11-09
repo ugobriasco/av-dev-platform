5f1a504b4a8e4ce4b424684d92af49dc09b1c418
Ugo <ugo.briasco@sumup.com>
2019-11-09 21:01:12 +0100

    Try using usb cable for com rpi-ino

--- src/opencv/main.cpp
+++ src/opencv/main.cpp
@@ -1,5 +1,7 @@
 /*
-*
+* AUTHONOMOUS VEHICLE DEV PLATFORM
+* Brain module for RPI
+* Author: Ugo Briasco
 */
 
 // Dependancies for Image processing
@@ -15,7 +17,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <sstream>
 
 // Namespaces
 using namespace std;
@@ -36,11 +38,16 @@ using namespace raspicam;
 #define CANNY_LOW_THRESHOLD_RATIO 500
 #define CANNY_KERNEL_SIZE 3
 
-
 //PARAMS - serial com
 #define CPORT 16
 #define BDRATE 57600 /* baud */
 #define BUF_SIZE 128
+
+//PARAMS - motion
+#define THRUST_MAX 40 //max thrust [%] of each motor
+#define THRUST_MIN 20 //min thrust [%] of each motor
+#define DEV_MAX 140 //max deviation ±140px from current bird's eye settings (±30deg)
+#define DEV_CUT 50 //deviation where convergence thrust should be max
 
 //GLOBAL image processing
 Mat frame, matrix, framePerspective, frameThreshold, frameCanny, frameFinal, frameFinalDuplicate;
@@ -65,6 +72,10 @@ Point2f Destination[] = {
 //GLOBAL serial com
 int poll;
 
+//GLOBAL motion
+std::string command;
+ostringstream buffer;
+
 
 // Setup camera
 void Setup (int argc, char **argv, RaspiCam_Cv &Camera){
@@ -75,6 +86,30 @@ void Setup (int argc, char **argv, RaspiCam_Cv &Camera){
 	Camera.set(CAP_PROP_SATURATION, ("-sa", argc, argv, SATURATION ));
 	Camera.set(CAP_PROP_GAIN, ("-g",argc,argv,50));
 	Camera.set(CAP_PROP_FPS, ("-fps",argc, argv, 0));
+}
+
+// Rotate image by a given angle
+Mat rotate(Mat src, double angle){
+    Mat dst;
+    Point2f pt(src.cols/2., src.rows/2.);
+    Mat r = getRotationMatrix2D(pt, angle, 1.0);
+    warpAffine(src, dst, r, Size(src.cols, src.rows));
+    return dst;
+}
+
+// Get image from raspicam
+void Capture(){
+	Camera.grab();
+	Camera.retrieve(frame);
+
+	// Transform image to RGB.
+	cvtColor(frame, frame, COLOR_BGR2RGB);
+
+	// The camera is placed upside-down, thus flip it.
+	flip(frame, frame,-1);
+
+  // Adjust offset
+  frame = rotate(frame, 3);
 }
 
 // Define region of interest
@@ -118,6 +153,7 @@ void Histogram(){
 	}
 }
 
+// Defines the deviiation between center of vehicles and a black lane on the floor
 void LaneFinder(){
 	vector<int>::iterator leftPtr;
 	leftPtr = max_element(histogramLane.begin(), histogramLane.begin() + 360);
@@ -132,28 +168,97 @@ void LaneFinder(){
 
 }
 
-// Rotate image by a given angle
-Mat rotate(Mat src, double angle){
-    Mat dst;
-    Point2f pt(src.cols/2., src.rows/2.);
-    Mat r = getRotationMatrix2D(pt, angle, 1.0);
-    warpAffine(src, dst, r, Size(src.cols, src.rows));
-    return dst;
+string IntToString(int a){
+    buffer << a;
+    return buffer.str();
 }
 
-// Get image from raspicam
-void Capture(){
-	Camera.grab();
-	Camera.retrieve(frame);
+// regulate the thrust of left/right engine
+void changeMotion(int left, int right){
+	command = "C2|";
 
-	// Transform image to RGB.
-	cvtColor(frame, frame, COLOR_BGR2RGB);
+	//Accept only int between 0 and 99
+	if(left < 0 || right<0 || left>99 || right>99){
+		return;
+	}
+	//parse to sting mantaining 2 digits
+	else if (left<10 && right<10){
+		command += "0";
+		command += IntToString(left);
+		command += "0";
+		command += IntToString(right);
+	}
 
-	// The camera is placed upside-down, thus flip it.
-	flip(frame, frame,-1);
+	else if(left<10 && right >=10){
+		command += "0";
+		command += IntToString(left);
+		command += IntToString(right);
+	}
 
-  // Adjust offset
-  frame = rotate(frame, 3);
+	else if(left>=10 && right < 10){
+		command += IntToString(left);
+		command += "0";
+		command += IntToString(right);
+	}
+
+	else {
+		command += IntToString(left);
+		command += IntToString(right);
+	}
+
+	//send command via serial
+	//RS232_cputs(CPORT,"C0|0000"); // sends string on serial
+
+	//log command
+	cout<<"rpi:"<<command<<endl;
+}
+
+int setConvergence(int deviation){
+	//TODO: calculate THRUST_MAX - deviation * (THRUST_MAX - THRUST_MIN)/DEV_CUT);
+	return 10;
+}
+
+void ConvergeToLane(){
+
+	// TODO improve algorithm
+	// //If deviation more then max deviation measured => lane lost: stop the vehicle
+	// if(abs(deviation) > DEV_MAX){
+	// 	changeMotion(0,0);
+	// 	cout<<"rpi: lane lost "<<endl;
+	// }
+	// //if deviation negative go left
+	// else if(deviation < 0 && abs(deviation) >= DEV_CUT){
+	// 	changeMotion(THRUST_MIN, THRUST_MAX);
+	// }
+	//
+	// else if(deviation < 0 && abs(deviation) < DEV_CUT){
+	// 	changeMotion(setConvergence(deviation), THRUST_MAX);
+	// }
+	// //if deviation positive go right
+	// else if(deviation > 0 && abs(deviation) >= DEV_CUT){
+	// 	changeMotion(THRUST_MAX, THRUST_MIN);
+	// }
+	//
+	// else if(deviation > 0 && abs(deviation) < DEV_CUT){
+	// 	changeMotion(THRUST_MAX, setConvergence(deviation));
+	// }
+
+	if(abs(deviation) > DEV_MAX){
+		RS232_cputs(CPORT,"C2|0000");
+		cout<<"rpi: lane lost "<<endl;
+	}
+	//if deviation negative go left
+	else if(deviation < 0){
+		RS232_cputs(CPORT,"C2|2050");
+	}
+	//if deviation positive go right
+	else if(deviation < 0){
+		RS232_cputs(CPORT,"C2|5020");
+	}
+	// if centered then move forward
+	else{
+		RS232_cputs(CPORT,"C2|5050");
+	}
 }
 
 // display what the av sees
@@ -185,13 +290,13 @@ void displayResults(){
 
 int main(int argc, char **argv){
 
-	//
-
+	//Serial com setup
 	char str_send[2][BUF_SIZE]; // send data buffer
 	unsigned char str_recv[BUF_SIZE]; // recv data buffer
 	int fd ;
 	char mode[]={'8','N','1',0}; // 8 data bits, no parity, 1 stop bit
 
+	//Camera setup
 	Setup(argc, argv, Camera);
 	cout<<"Connecting to camera"<<endl;
 	if (!Camera.open()){
@@ -207,11 +312,11 @@ int main(int argc, char **argv){
 		 return(0);
 	 }
 
-
-	// Capturing Video
-	usleep(2000000);
+	usleep(2000000); /* waits 2000ms for stable condition */
+	RS232_cputs(CPORT,"C0|0000"); // sends string on serial
+	cout<<"rpi:get device info"<<endl;
 	while(1) {
-		//auto start = std::chrono::system_clock::now();
+		auto start = std::chrono::system_clock::now();
 
 		// Image processing
 		Capture();
@@ -219,23 +324,32 @@ int main(int argc, char **argv){
 		Threshold();
 		Histogram();
 		LaneFinder();
+		displayResults();
+
+		//Motion controller
+		RS232_cputs(CPORT,"C3|5050"); // sends string on serial
+		usleep(2000000);
+
+
 
 		//Serial Com
 		poll = RS232_PollComport(CPORT, str_recv, (int)BUF_SIZE);
 		if(poll > 0){
 	      str_recv[poll] = 0;   /* always put a "null" at the end of a string! */
-	      printf("ino-in[%i bytes]%s", poll, (char *)str_recv);
+	      printf("ino[%i bytes]%s", poll, (char *)str_recv);
 		}
+
+
+		//ConvergeToLane();
+		//usleep(500);
 
 		//Log FPS
 		// auto end = std::chrono::system_clock::now();
 		// std::chrono::duration<double> elapsed_seconds = end-start;
 		// float t = elapsed_seconds.count();
 		// int FPS = 1/t;
-		// cout<<"FPS= "<<FPS<<endl;
+		// cout<<"cam-i "<<FPS<<" fps"<<endl;
 
-
-		displayResults();
 		waitKey(1);
 	}
 
